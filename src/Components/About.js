2b1d684c9fe6e8b00e586d58321df1737bdb7d1d
Sumit <sumitkumar280601@gmail.com>
2021-12-27 18:52:07 +0530

    Fixing some issue

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,18 +8,21 @@ export default function About() {
 
   return (
     <div className="container">
-      <div className="card w-75">
+      <div className="card w-90">
         <div className="card-body">
           <h3 className="card-title">Your Notes are Saves here</h3>
           <p className="card-text">Write your Notes here and and its saves with Us</p>
         </div>
       </div>
 
-      <div className="card w-75 my-5">
+      <div className="card w-90 my-5">
         <div className="card-body">
-          <h3 className="card-title">You can Contact Us</h3>
-          <div> <a href="mailto:sumit280601@gmail.com"><i className="far fa-envelope fa-3x mx-3"></i> </a>
-            <a href="https://www.linkedin.com/in/sumit-kumar-03b99a210/" target="_blank"><i className="fab fa-linkedin fa-3x"></i> </a> </div>
+          <h3 className="card-title ">You can Contact Us</h3>
+          <div className='mt-4'> 
+            <a href="mailto:sumit280601@gmail.com"><i className="far fa-envelope fa-3x mx-4"></i> </a>
+            <a href="https://www.linkedin.com/in/sumit-kumar-03b99a210/" rel="noreferrer" target="_blank"><i className="fab fa-linkedin fa-3x"></i> </a>
+            <a href="https://twitter.com/SumitKu55624025" rel="noreferrer" target="_blank"><i className="fab fa-twitter fa-3x mx-4"></i></a>
+             </div>
         </div>
       </div>
 
